395e0c6d8ad734c2ac9c523d71ad10f470b4711f
Tanmayi tadala <tanmayitadala@gmail.com>
2024-12-13 14:45:13 +0530

    UC2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        String sentence = "To be or not to be";
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
         HashTable hashTable = new HashTable(10);
-        String[] words = sentence.split(" ");
+        String[] words = paragraph.split(" ");
         for (String word : words) {
-            word = word.toLowerCase();
-            hashTable.put(word, 1);
+            hashTable.put(word,1);
         }
-        System.out.println("frequency of words in a sentence 'To be or not to be' : ");
+        System.out.println("Frequency of words:");
         hashTable.printHashTable();
     }
 }
